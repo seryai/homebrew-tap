8bef7dba8a0df4ae76fec2738a82f7a877b128f5
longniao <longniao@gmail.com>
2026-05-16 17:04:07 -0500

    chore: bump version to 0.8.8 (sha256 TBD after release)

--- Casks/sery-link.rb
+++ Casks/sery-link.rb
@@ -1,5 +1,5 @@
 cask "sery-link" do
-  version "0.8.7"
+  version "0.8.8"
 
   on_arm do
     url "https://github.com/seryai/sery-link/releases/download/v#{version}/Sery.Link_#{version}_aarch64.dmg"
