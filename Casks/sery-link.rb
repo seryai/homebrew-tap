8a640fe4bec054c867b1b5b5936e507fcb953400
longniao <longniao@gmail.com>
2026-05-19 15:45:42 -0500

    chore: bump version to 0.9.4 (sha256 TBD after release)

--- Casks/sery-link.rb
+++ Casks/sery-link.rb
@@ -1,5 +1,5 @@
 cask "sery-link" do
-  version "0.9.3"
+  version "0.9.4"
 
   on_arm do
     url "https://github.com/seryai/sery-link/releases/download/v#{version}/Sery.Link_#{version}_aarch64.dmg"
