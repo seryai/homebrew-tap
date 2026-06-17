17822f5f965df0402e1ba33967350d5edc22fe4d
longniao <longniao@gmail.com>
2026-06-17 08:36:23 -0500

    chore: update sha256 for v0.12.12

--- Casks/sery-link.rb
+++ Casks/sery-link.rb
@@ -3,12 +3,12 @@ cask "sery-link" do
 
   on_arm do
     url "https://github.com/seryai/sery-link/releases/download/v#{version}/Sery.Link_#{version}_aarch64.dmg"
-    sha256 "022a89c487ff24dcb8629413d461f727d7ac08887d0fb480d2c802da279fdb83"
+    sha256 "08ceea45b876e7c6f237df034ca7d64494730a20e2cb7c4e8f396555eac3215d"
   end
 
   on_intel do
     url "https://github.com/seryai/sery-link/releases/download/v#{version}/Sery.Link_#{version}_x64.dmg"
-    sha256 "551d9910569a93b2c9f046705807ceb4cf9e28386864aad325020e0f99b29392"
+    sha256 "e07889af1ef5bd22e74bba5019becd0baf0b8ae96ac5758a8eaafb1a8949b0aa"
   end
 
   name "Sery Link"
